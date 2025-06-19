35a59ebe56c700592eb4f75b2733c6def1c56480
LittleT0fu <chinchanathavornkraiwit@gmail.com>
2025-06-19 13:52:14 +0700

    Revert "checkpoint"
    This reverts commit 527975f74ae851131102ecaff76a5c178ec84962.

--- controllers/userController.js
+++ controllers/userController.js
@@ -293,15 +293,10 @@ exports.deleteUser = async (req, res) => {
     }
 };
 
-// login
-//@route POST /users/login
-//@access Public
 exports.login = async (req, res) => {
     try {
-        // get value from request
         const { email, password } = req.body;
 
-        // find user by email
         const user = await userModel.findByEmail(email);
         if (!user) {
             // create login log when login failed
