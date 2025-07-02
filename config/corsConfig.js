216889bab836d5ca3b2ba4a4ff44402b5c321adb
LittleT0fu <chinchanathavornkraiwit@gmail.com>
2025-07-03 00:28:33 +0700

    Update CORS headers and Docker start command
    Added 'x-csrf-token' to allowed CORS headers for improved security.
    
    Changed the Docker Compose app service command from 'npm run dev' to 'npm run start' to use the production start script.

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -3,7 +3,7 @@ const config = require("./env");
 const corsOptions = {
     origin: config.allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
     credentials: true,
     maxAge: 86400, // 24 hours
     preflightContinue: false,
